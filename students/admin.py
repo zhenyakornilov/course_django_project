4008ceb3f323441b976667b82d14457c5a8dfb83
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2021-08-15 23:17:20 +0300

    Improved admin panel

--- students/admin.py
+++ students/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Student
 
-admin.site.register(Student)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "age")
+    list_filter = ("age", "last_name", 'first_name')
+    search_fields = ("last_name__startswith", )
--- teachers/admin.py
+++ teachers/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Teacher
 
-admin.site.register(Teacher)
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ("subject", "last_name", "first_name", "age")
+    list_filter = ("age", "last_name", "subject")
+    search_fields = ("last_name__startswith", "first_name__startswith")

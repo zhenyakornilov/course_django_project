34888b853f1befe3a9506a9a10496ddadec1de2b
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2021-09-03 23:40:17 +0300

    Travis CI check test

--- django_kornilov/settings.py
+++ django_kornilov/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+
 from pathlib import Path
+import os
 
 from celery.schedules import crontab
 

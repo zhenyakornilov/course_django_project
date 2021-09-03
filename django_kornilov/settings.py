f263e80fd53979efd4b5725126d1cc05b2ac1dd5
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2021-09-03 23:19:29 +0300

    Update project files

--- django_kornilov/settings.py
+++ django_kornilov/settings.py
@@ -151,8 +151,7 @@ CELERY_BEAT_SCHEDULE = {
 }
 
 # Sending e-mail settings
-
-ADMINS_EMAIL = ['zhenyakornilov98@gmail.com']
+ADMINS_EMAIL = ['zhenyakornilov98@gmail.com', 'vitalik1996@gmail.com']
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True

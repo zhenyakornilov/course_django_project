692f38c041a38033f56bc887f83fee9f167afbcf
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2021-09-10 19:47:14 +0300

    Model relations test

--- group/models.py
+++ group/models.py
@@ -5,10 +5,10 @@ class Group(models.Model):
     group_name = models.CharField('Group Name', max_length=200, db_column='Group name')
     students_in_group = models.IntegerField('Students in group', db_column='Students in group')
 
-    group_curator = models.ForeignKey("teachers.Teacher", null=True,
-                                      on_delete=models.CASCADE, db_column='Group curator')
-    group_monitor = models.ForeignKey("students.Student", null=True,
-                                      on_delete=models.CASCADE, db_column='Group monitor')
+    group_curator = models.OneToOneField("teachers.Teacher", null=True,
+                                         on_delete=models.CASCADE, db_column='Group curator ID')
+    group_monitor = models.OneToOneField("students.Student", null=True,
+                                         on_delete=models.CASCADE, db_column='Group monitor ID')
 
     def __str__(self):
         return self.group_name, self.students_in_group, self.group_curator.first_name
--- teachers/management/commands/generate-teachers.py
+++ teachers/management/commands/generate-teachers.py
@@ -1,11 +1,12 @@
-from random import randint
-
 from django.core.management.base import BaseCommand
 
 from faker import Faker
 
 from teachers.models import Teacher
 
+from students.models import Student
+
+from group.models import Group
 
 teacher_subjects = ['Math', 'Physics', 'Chemistry', 'History',
                     'Music', 'Spanish', 'English', 'Computing', 'Geography']
@@ -19,11 +20,25 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         fake = Faker()
-
+        result = []
         for i in range(options['number_of_teachers']):
-            Teacher.objects.create(subject=teacher_subjects[randint(0, 8)],
-                                   first_name=fake.first_name(),
-                                   last_name=fake.last_name(),
-                                   age=fake.random_int(27, 60))
+            teacher = Teacher(subject=teacher_subjects[fake.random_int(0, 8)],
+                              first_name=fake.first_name(),
+                              last_name=fake.last_name(),
+                              age=fake.random_int(27, 60))
+            teacher.save()
+
+            for student in range(fake.random_int(0, 11)):
+                student = Student(first_name=fake.first_name(),
+                                  last_name=fake.last_name(),
+                                  age=fake.random_int(18, 26))
+                result.append(student)
+            Student.objects.bulk_create(result)
+
+            group = Group(group_name=teacher_subjects[fake.random_int(0, 8)],
+                          group_curator=teacher,
+                          group_monitor=Student.objects.last(),
+                          students_in_group=len(result))
+            group.save()
 
         self.stdout.write(self.style.SUCCESS(f"Successfully created {options['number_of_teachers']} teacher(s)"))

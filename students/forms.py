c0adacedbf4e9f537b35afd3be4490793c090010
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2021-10-07 16:09:07 +0300

    Some tests done

--- students/forms.py
+++ students/forms.py
@@ -21,14 +21,6 @@ class StudentForm(forms.ModelForm):
         model = Student
         fields = ['first_name', 'last_name', 'age', 'phone_number']
 
-    # prev version of form
-    # first_name = forms.CharField(label='Student\'s first name', required=True, max_length=200)
-    # last_name = forms.CharField(label='Student\'s last name', required=True, max_length=200)
-    # age = forms.IntegerField(label='Student\'s age', required=True)
-    # phone_number = forms.CharField(label='Student\'s phone', required=False,
-    #                                empty_value=None, validators=[validate_phone_number],
-    #                                widget=forms.TextInput(attrs={'placeholder': '380xxxxxxxxx'}))
-
 
 class GenerateStudentsForm(forms.Form):
     total = forms.IntegerField(
--- students/tests/students_tests.py
+++ students/tests/students_tests.py
@@ -1,29 +1,57 @@
-from django.test import Client
-
 import pytest
 
+from pytest_django.asserts import assertTemplateUsed
+
+from students.forms import StudentForm
 from students.models import Student
 from students.tasks import generate_random_students
 
 
 @pytest.mark.django_db
-def test_new_student(create_student):
+@pytest.mark.parametrize('path', [
+    '/',
+    '/all-students/',
+])
+def test_students_views(path, client, create_student):
     assert Student.objects.count() == 1
-    c = Client()
-    get_main = c.get('/all-students/')
-    assert get_main.status_code == 200
+    response = client.get(path)
+    assert response.status_code == 200
+    if path == '/':
+        assertTemplateUsed(response, 'students/index.html')
+    if path == '/all-students/':
+        assertTemplateUsed(response, 'students/students_list.html')
     assert Student.objects.count() == 1
 
 
 @pytest.mark.django_db
-def test_main_page(create_student):
-    c = Client()
-    get_main = c.get('/')
-    assert get_main.status_code == 200
-    assert Student.objects.count() == 1
-
-
-@pytest.mark.django_db
-def test_generate_students():
+def test_generate_random_students(create_student):
     assert generate_random_students(3) == '3 random students created with success!'
-    assert Student.objects.count() == 3
+    assert Student.objects.count() == 4
+
+
+def test_admin_panel_view(admin_client):
+    response = admin_client.get('/admin/')
+    assert response.status_code == 200
+
+
+class TestStudentCreationForm:
+    def test_form_is_invalid(self):
+        form = StudentForm(data={'first_name': 'test', 'last_name': 'test',
+                                 'age': 24, 'phone_number': 'bad_number'})
+        assert not form.is_valid()
+
+    @pytest.mark.django_db
+    def test_form_is_valid(self):
+        form = StudentForm(data={'first_name': 'test', 'last_name': 'test',
+                                 'age': 24, 'phone_number': '380000000000'})
+        assert form.is_valid()
+
+    def test_phone_validation(self):
+        form = StudentForm(data={'first_name': 'test', 'last_name': 'test',
+                                 'age': 24, 'phone_number': '355663809280'})
+        assert not form.is_valid()
+
+    def test_form_is_empty(self):
+        form = StudentForm(data={})
+        assert not form.is_valid()
+        assert len(form.errors) == 3

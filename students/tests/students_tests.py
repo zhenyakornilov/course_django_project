dd5617a2a453c2f1ebf31cb7c33e5bff3148dca6
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2021-10-08 17:29:19 +0300

    Some tests done

--- students/tests/students_tests.py
+++ students/tests/students_tests.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pytest_django.asserts import assertTemplateUsed
+from pytest_django.asserts import assertTemplateUsed, assertFormError
 
 from students.forms import StudentForm
 from students.models import Student
@@ -11,16 +11,55 @@ from students.tasks import generate_random_students
 @pytest.mark.parametrize('path', [
     '/',
     '/all-students/',
+    '/edit-student/'
 ])
 def test_students_views(path, client, create_student):
-    assert Student.objects.count() == 1
+    if path == '/edit-student/':
+        student = Student.objects.get(pk=1)
+        path = f'/edit-student/{student.pk}/'
+        response = client.post(path, data={'first_name': 'test', 'last_name': 'test',
+                                           'age': 24, 'phone_number': '380000000000'},
+                               follow=True)
+        redirect_url = response.redirect_chain[0][0]
+        print(redirect_url)
+        redirect_status_code = response.redirect_chain[0][1]
+
+        assert redirect_url == '/all-students/'
+        assert redirect_status_code == 302
+
     response = client.get(path)
     assert response.status_code == 200
     if path == '/':
         assertTemplateUsed(response, 'students/index.html')
     if path == '/all-students/':
+        student = Student.objects.get(pk=1)
+        assert student.first_name in response.content.decode()
         assertTemplateUsed(response, 'students/students_list.html')
     assert Student.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_edit_student_view(request, client, create_student):
+    response_get = client.get('/edit-student/1/')
+    # assertTemplateUsed(response_get, 'students/student-edit-form.html')
+    # invalid_data = {'first_name': 'test', 'last_name': 'test',
+    #                 'age': 24, 'phone_number': 'bad_number'}
+    # expected_error = f"{invalid_data.get('phone_number')}: Enter phone number in 380xxxxxxxxx format"
+    # form = StudentForm()
+    # response = client.post('/edit-student/1/', data=invalid_data)
+    # assertFormError(response, form, 'phone_number', expected_error)
+
+
+@pytest.mark.django_db
+def test_edit_student(client, create_student):
+    response = client.get('/edit-student/1/')
+    assert response.status_code == 200
+    # assertTemplateUsed(response, 'students/student_edit_form.html')
+    # form = StudentForm()
+    # invalid_data = {'first_name': 'test', 'last_name': 'test', 'age': 24, 'phone_number': 'bad_number'}
+    # expected_error = f'{invalid_data.get("phone_number")}: Enter phone number in 380xxxxxxxxx format'
+    # response_edit = client.post('/edit-student/1/', data=invalid_data)
+    # assertFormError(response, form, 'phone_number', expected_error)
 
 
 @pytest.mark.django_db
@@ -55,3 +94,9 @@ class TestStudentCreationForm:
         form = StudentForm(data={})
         assert not form.is_valid()
         assert len(form.errors) == 3
+
+
+def test_custom_error_404(client):
+    response = client.get('/non-existent-url/')
+    assert response.status_code == 404
+    assertTemplateUsed(response, './errors/404_error_handler.html')

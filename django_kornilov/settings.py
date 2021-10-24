bb61a9c73222d1b42cb3520220185e594fd4bda3
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2021-10-24 17:39:16 +0300

    Create registration view with email confirmation

--- django_kornilov/settings.py
+++ django_kornilov/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", 'TEST')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["zhenyakornilov.herokuapp.com", "127.0.0.1", "localhost"]
 
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "group.apps.GroupConfig",
     "mail_processing.apps.MailProcessingConfig",
     "currency.apps.CurrencyConfig",
+    "user_signup.apps.UserSignupConfig",
 ]
 
 MIDDLEWARE = [
---
+++ user_signup/views.py
@@ -0,0 +1,75 @@
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.views.generic import View
+
+
+from django_kornilov.settings import EMAIL_HOST_USER
+
+from .forms import SignUpForm
+
+
+class SignUpView(View):
+    form_class = SignUpForm
+    template_name = 'user_signup/signup.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+
+            current_site = get_current_site(request)
+            subject = 'Activate Your MySite Account'
+            message = render_to_string('user_signup/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                subject, message, to=[to_email],
+                from_email=f"DJANGO_KORNILOV {EMAIL_HOST_USER}"
+            )
+            email.send()
+
+            messages.success(request, 'Please Confirm your email to complete registration.')
+
+            return redirect('main-page')
+
+        return render(request, self.template_name, {'form': form})
+
+
+class ActivateAccount(View):
+    def get(self, request, uidb64, token, *args, **kwargs):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and default_token_generator.check_token(user, token):
+            user.is_active = True
+            user.profile.email_confirmed = True
+            user.profile.save()
+            user.save()
+            login(request, user)
+            messages.success(request, 'Your account have been confirmed.')
+            return redirect('main-page')
+        else:
+            messages.warning(request, 'The confirmation link was invalid, possibly because it has already been used.')
+            return redirect('main-page')

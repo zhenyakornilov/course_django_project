6bd88d03d7e47a313b48deb81bdf7ab1e7aa835f
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2021-11-02 18:08:11 +0200

    set settings.py DEBUG=False

--- django_kornilov/settings.py
+++ django_kornilov/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", 'TEST')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["zhenyakornilov.herokuapp.com", "127.0.0.1", "localhost"]
 
